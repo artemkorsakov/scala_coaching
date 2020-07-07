198de21dac162541b4ab67bba1ec7ec068b99247
Artem Korsakov <kor@infra.guru>
2020-07-07 11:00:59 +0300

    Add EitherImp first version

--- src/main/scala/EitherImp.scala
+++ src/main/scala/EitherImp.scala
@@ -1,3 +1,137 @@
-object EitherImp {
-  def hello() = "Hello, World!"
+sealed trait EitherImp[+L, +R] extends Product with Serializable {
+
+  def isLeft: Boolean
+
+  def isRight: Boolean
+
+  /** Projects this `Either` as a `Right`.
+   */
+  def right: RightProjectionImp[L, R] = RightProjectionImp(this)
+
+  /** The given function is applied if this is a `Right`.
+   *
+   * {{{
+   *  Right(12).map(x => "flower") // Result: Right("flower")
+   *  Left(12).map(x => "flower")  // Result: Left(12)
+   * }}}
+   */
+  def map[R1](f: R => R1): EitherImp[L, R1] = this match {
+    case RightImp(b) => RightImp(f(b))
+    case _ => this.asInstanceOf[EitherImp[L, R1]]
+  }
+
+  /** Binds the given function across `Right`.
+   *
+   * @param f The function to bind across `Right`.
+   */
+  def flatMap[L1 >: L, R1](f: R => EitherImp[L1, R1]): EitherImp[L1, R1] = this match {
+    case RightImp(b) => f(b)
+    case _ => this.asInstanceOf[EitherImp[L1, R1]]
+  }
+
+  /** Binds the given function across `Left`.
+   *
+   * {{{
+   *  Left(12).left.flatMap(x => Left("scala")) // Left("scala")
+   *  Right(12).left.flatMap(x => Left("scala")) // Right(12)
+   * }}}
+   *
+   * @param f The function to bind across `Left`.
+   */
+  def leftFlatMap[L1, R1 >: R](f: L => EitherImp[L1, R1]): EitherImp[L1, R1] = this.left.flatMap(f)
+
+  /** Maps the function argument through `Left`.
+   *
+   * {{{
+   *  Left(12).left.map(_ + 2) // Left(14)
+   *  Right[Int, Int](12).left.map(_ + 2) // Right(12)
+   * }}}
+   */
+  def leftMap[L1](f: L => L1): EitherImp[L1, R] = left.map(f)
+
+  /** Projects this `Either` as a `Left`.
+   */
+  def left: LeftProjectionImp[L, R] = LeftProjectionImp(this)
+
 }
+
+case class RightImp[R](value: R) extends EitherImp[Nothing, R] {
+  override def isLeft = false
+
+  override def isRight = true
+}
+
+final case class RightProjectionImp[+L, +R](e: EitherImp[L, R]) {
+
+  /** Returns the value from this `Right` or throws
+   * `java.util.NoSuchElementException` if this is a `Left`.
+   *
+   * {{{
+   *  Right(12).right.get // 12
+   *  Left(12).right.get // NoSuchElementException
+   * }}}
+   *
+   * @throws java.util.NoSuchElementException if the projection is `Left`.
+   */
+  def get: R = e match {
+    case RightImp(b) => b
+    case _ => throw new NoSuchElementException("Either.right.get on Left")
+  }
+}
+
+case class LeftImp[L](value: L) extends EitherImp[L, Nothing] {
+  override def isLeft = true
+
+  override def isRight = false
+}
+
+/** Projects an `Either` into a `Left`.
+ *
+ * @author <a href="mailto:research@workingmouse.com">Tony Morris</a>, Workingmouse
+ * @see [[scala.util.Either#left]]
+ */
+final case class LeftProjectionImp[+L, +R](e: EitherImp[L, R]) {
+  /** Returns the value from this `Left` or throws `java.util.NoSuchElementException`
+   * if this is a `Right`.
+   *
+   * {{{
+   *  Left(12).left.get  // 12
+   *  Right(12).left.get // NoSuchElementException
+   * }}}
+   *
+   * @throws java.util.NoSuchElementException if the projection is [[scala.util.Right]]
+   */
+  def get: L = e match {
+    case LeftImp(a) => a
+    case _ => throw new NoSuchElementException("Either.left.get on Right")
+  }
+
+  /** Binds the given function across `Left`.
+   *
+   * {{{
+   *  Left(12).left.flatMap(x => Left("scala")) // Left("scala")
+   *  Right(12).left.flatMap(x => Left("scala")) // Right(12)
+   * }}}
+   *
+   * @param f The function to bind across `Left`.
+   */
+  def flatMap[L1, R1 >: R](f: L => EitherImp[L1, R1]): EitherImp[L1, R1] = e match {
+    case LeftImp(a) => f(a)
+    case _ => e.asInstanceOf[EitherImp[L1, R1]]
+  }
+
+  /** Maps the function argument through `Left`.
+   *
+   * {{{
+   *  Left(12).left.map(_ + 2) // Left(14)
+   *  Right[Int, Int](12).left.map(_ + 2) // Right(12)
+   * }}}
+   */
+  def map[L1](f: L => L1): EitherImp[L1, R] = e match {
+    case LeftImp(a) => LeftImp(f(a))
+    case _ => e.asInstanceOf[EitherImp[L1, R]]
+  }
+
+}
+
+// Right#apply, Left#apply
--- src/test/scala/EitherImpTest.scala
+++ src/test/scala/EitherImpTest.scala
@@ -2,8 +2,16 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class EitherImpTest extends AnyFlatSpec {
 
-  "EitherImp.hello" should "return \"Hello, World!\" text" in {
-    assert(EitherImp.hello() === "Hello, World!")
+  "EitherImp.isLeft.isRight" should "returns isRight === true and isLeft === false if it is RightImp" in {
+    val either: EitherImp[String, Int] = RightImp(1)
+    assert(either.isLeft === false)
+    assert(either.isRight === true)
+  }
+
+  "EitherImp.isLeft.isRight" should "returns isRight === false and isLeft === true if it is LeftImp" in {
+    val either: EitherImp[String, Int] = LeftImp("Test")
+    assert(either.isLeft === true)
+    assert(either.isRight === false)
   }
 
 }
