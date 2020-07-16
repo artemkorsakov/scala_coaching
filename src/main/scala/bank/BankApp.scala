cd15890fd559819aa727ab68cdde85fcafeeff05
Artem Korsakov <kor@infra.guru>
2020-07-16 11:30:20 +0300

    Add tagless final realization

--- src/main/scala/bank/BankApp.scala
+++ src/main/scala/bank/BankApp.scala
@@ -1,71 +1,28 @@
 package bank
 
 import bank.BankService._
-
-import scala.collection.mutable
+import bank.BankServiceDsl._
 
 object BankApp {
-  class BankApplication extends BankService[Either] with IdGenerator {
-    private val users: mutable.HashMap[String, UserId] = mutable.HashMap.empty[String, UserId]
-    private val accounts: mutable.HashMap[UserId, (AccountId, Balance)] = mutable.HashMap.empty[UserId, (AccountId, Balance)]
 
-    override def addUser(name: String): Boolean = if (users.contains(name)) false else {
-      users += (name -> newId)
-      true
-    }
+  class BankApplication extends BankService[My] {
+    override def addUser(name: String): Boolean = BankService.addUser[My](name)
 
-    override def getUserIdByName(name: String): Either[UserError, UserId] =
-      if (users.contains(name)) Right(users(name)) else Left(NameNotFound(name))
+    override def getUserIdByName(name: String): My[UserError, UserId] = BankService.getUserIdByName[My](name)
 
-    override def getAllUsersIds: Either[UserError, List[UserId]] =
-      if (users.isEmpty) Left(new UserListIsEmpty) else Right(users.values.toList)
+    override def getAllUsersIds: My[UserError, List[UserId]] = BankService.getAllUsersIds[My]
 
-    override def getAllUsersNames: Either[UserError, List[String]] =
-      if (users.isEmpty) Left(new UserListIsEmpty) else Right(users.keySet.toList)
+    override def getAllUsersNames: My[UserError, List[String]] = BankService.getAllUsersNames[My]
 
-    override def createAccount(userId: UserId): Either[AccountingError, AccountId] =
-      if (accounts.contains(userId)) {
-        Left(UserAlreadyHasAccount(userId))
-      } else if (!getAllUsersIds.getOrElse(List.empty[UserId]).contains(userId)) {
-        Left(UserNotFound(userId))
-      } else {
-        val id = newId
-        accounts += (userId -> (id, 0.0))
-        Right(id)
-      }
+    override def createAccount(userId: UserId): My[AccountingError, AccountId] = BankService.createAccount[My](userId)
 
-    override def getAccountIdByUser(userId: UserId): Either[AccountingError, AccountId] =
-      if (accounts.contains(userId)) Right(accounts(userId)._1) else Left(UserNotFound(userId))
+    override def getAccountIdByUser(userId: UserId): My[AccountingError, AccountId] = BankService.getAccountIdByUser[My](userId)
 
-    override def balance(userId: UserId): Either[AccountingError, Balance] =
-      if (accounts.contains(userId)) Right(accounts(userId)._2) else Left(UserNotFound(userId))
+    override def balance(userId: UserId): My[AccountingError, Balance] = BankService.balance[My](userId)
 
-    override def put(userId: UserId, amount: BigDecimal): Either[AccountingError, Balance] =
-      if (amount < 0) {
-        Left(AmountLessThanZero(amount))
-      } else if (!accounts.contains(userId)) {
-        Left(UserNotFound(userId))
-      } else {
-        changeBalance(userId, amount)
-      }
+    override def put(userId: UserId, amount: BigDecimal): My[AccountingError, Balance] = BankService.put[My](userId, amount)
 
-    override def charge(userId: UserId, amount: BigDecimal): Either[AccountingError, Balance] =
-      if (!accounts.contains(userId)) {
-        Left(UserNotFound(userId))
-      } else {
-        val balance: Balance = accounts(userId)._2
-        if (balance < amount) {
-          Left(BalanceTooLow(balance))
-        } else {
-          changeBalance(userId, -amount)
-        }
-      }
-
-    private def changeBalance(userId: UserId, amount: BigDecimal): Either[AccountingError, Balance] = {
-      val newAmount: Balance = accounts(userId)._2 + amount
-      accounts(userId) = (accounts(userId)._1, newAmount)
-      balance(userId)
-    }
+    override def charge(userId: UserId, amount: BigDecimal): My[AccountingError, Balance] = BankService.charge[My](userId, amount)
 
     def balance(name: String): Either[Error, Balance] = {
       for {
---
+++ src/main/scala/bank/BankServiceDsl.scala
@@ -0,0 +1,72 @@
+package bank
+
+import bank.BankService._
+
+import scala.collection.mutable
+
+object BankServiceDsl extends IdGenerator {
+  type My[+A, +B] = Either[A, B]
+
+  implicit val myDsl: BankService[My] = new BankService[My] {
+    private val users: mutable.HashMap[String, UserId] = mutable.HashMap.empty[String, UserId]
+    private val accounts: mutable.HashMap[UserId, (AccountId, Balance)] = mutable.HashMap.empty[UserId, (AccountId, Balance)]
+
+    override def addUser(name: String): Boolean = if (users.contains(name)) false else {
+      users += (name -> newId)
+      true
+    }
+
+    override def getUserIdByName(name: String): My[UserError, UserId] =
+      if (users.contains(name)) Right(users(name)) else Left(NameNotFound(name))
+
+    override def getAllUsersIds: My[UserError, List[UserId]] =
+      if (users.isEmpty) Left(new UserListIsEmpty) else Right(users.values.toList)
+
+    override def getAllUsersNames: My[UserError, List[String]] =
+      if (users.isEmpty) Left(new UserListIsEmpty) else Right(users.keySet.toList)
+
+    override def createAccount(userId: UserId): My[AccountingError, AccountId] =
+      if (accounts.contains(userId)) {
+        Left(UserAlreadyHasAccount(userId))
+      } else if (!getAllUsersIds.getOrElse(List.empty[UserId]).contains(userId)) {
+        Left(UserNotFound(userId))
+      } else {
+        val id = newId
+        accounts += (userId -> (id, 0.0))
+        Right(id)
+      }
+
+    override def getAccountIdByUser(userId: UserId): My[AccountingError, AccountId] =
+      if (accounts.contains(userId)) Right(accounts(userId)._1) else Left(UserNotFound(userId))
+
+    override def balance(userId: UserId): My[AccountingError, Balance] =
+      if (accounts.contains(userId)) Right(accounts(userId)._2) else Left(UserNotFound(userId))
+
+    override def put(userId: UserId, amount: BigDecimal): My[AccountingError, Balance] =
+      if (amount < 0) {
+        Left(AmountLessThanZero(amount))
+      } else if (!accounts.contains(userId)) {
+        Left(UserNotFound(userId))
+      } else {
+        changeBalance(userId, amount)
+      }
+
+    override def charge(userId: UserId, amount: BigDecimal): My[AccountingError, Balance] =
+      if (!accounts.contains(userId)) {
+        Left(UserNotFound(userId))
+      } else {
+        val balance: Balance = accounts(userId)._2
+        if (balance < amount) {
+          Left(BalanceTooLow(balance))
+        } else {
+          changeBalance(userId, -amount)
+        }
+      }
+
+    private def changeBalance(userId: UserId, amount: BigDecimal): Either[AccountingError, Balance] = {
+      val newAmount: Balance = accounts(userId)._2 + amount
+      accounts(userId) = (accounts(userId)._1, newAmount)
+      balance(userId)
+    }
+  }
+}
